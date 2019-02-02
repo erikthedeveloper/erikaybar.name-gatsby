dad4246f1cf4924f6c9963e4a3c2a9e022985b88
Erik Aybar <erikthedeveloper@gmail.com>
2019-02-02 10:31:06 -0700

    Update homepage meta title "Welcome"

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="All posts"
+          title="Welcome"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <Bio />
