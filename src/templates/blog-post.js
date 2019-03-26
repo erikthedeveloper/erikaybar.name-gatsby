abab264a16534107cc1756da207d3dce808ddbf8
Erik Aybar <erikthedeveloper@gmail.com>
2019-03-25 18:58:06 -0600

    Extract AdjacentPosts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Link, graphql} from 'gatsby'
-
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
@@ -27,43 +26,59 @@ class BlogPostTemplate extends React.Component {
           {post.frontmatter.date}
         </p>
         <div dangerouslySetInnerHTML={{__html: post.html}} />
+        <AdjacentPosts previous={previous} next={next} />
         <hr
           style={{
             marginBottom: rhythm(1),
           }}
         />
         <Bio />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
       </Layout>
     )
   }
 }
 
 export default BlogPostTemplate
+
+function AdjacentPosts({previous, next}) {
+  if (!previous && !next) {
+    return null
+  }
+  return (
+    <>
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <ul
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              {' '}
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →{' '}
+            </Link>
+          )}
+        </li>
+      </ul>
+    </>
+  )
+}
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
