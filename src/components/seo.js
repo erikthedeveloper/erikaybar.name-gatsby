452827ee337468f7c1155232236720d3ab976708
Erik Aybar <erikthedeveloper@gmail.com>
2019-04-17 06:33:55 -0600

    Refactor SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,68 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import {StaticQuery, graphql} from 'gatsby'
+import {useSiteMetadata} from '../graphql/component-queries'
 
-function SEO({description, lang, meta, keywords, title}) {
+export default function SEO({description, lang, meta, keywords, title}) {
+  const siteMetadata = useSiteMetadata()
   return (
-    <StaticQuery
-      query={detailsQuery}
-      render={data => {
-        const metaDescription =
-          description || data.site.siteMetadata.description
-        return (
-          <Helmet
-            htmlAttributes={{
-              lang,
-            }}
-            title={title}
-            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-            meta={[
-              {
-                name: `description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:title`,
-                content: title,
-              },
-              {
-                property: `og:description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:type`,
-                content: `website`,
-              },
-              {
-                name: `twitter:card`,
-                content: `summary`,
-              },
-              {
-                name: `twitter:creator`,
-                content: data.site.siteMetadata.author,
-              },
-              {
-                name: `twitter:title`,
-                content: title,
-              },
-              {
-                name: `twitter:description`,
-                content: metaDescription,
-              },
-            ]
-              .concat(
-                keywords.length > 0
-                  ? {
-                      name: `keywords`,
-                      content: keywords.join(`, `),
-                    }
-                  : []
-              )
-              .concat(meta)}
-          />
-        )
+    <Helmet
+      htmlAttributes={{
+        lang,
       }}
+      title={title}
+      titleTemplate={`%s | ${siteMetadata.title}`}
+      meta={[
+        ...defaultMeta({
+          ...siteMetadata,
+          description: description || siteMetadata.description,
+          title,
+          keywords,
+        }),
+        ...meta,
+      ]}
     />
   )
 }
@@ -81,16 +39,43 @@ SEO.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
-export default SEO
-
-const detailsQuery = graphql`
-  query DefaultSEOQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        author
-      }
-    }
-  }
-`
+function defaultMeta({description, title, keywords, author}) {
+  return [
+    {
+      name: `description`,
+      content: description,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: description,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: author,
+    },
+    {
+      name: `twitter:title`,
+      content: title,
+    },
+    {
+      name: `twitter:description`,
+      content: description,
+    },
+    {
+      name: `keywords`,
+      content: keywords.join(`, `),
+    },
+  ].filter(meta => Boolean(meta.content))
+}
