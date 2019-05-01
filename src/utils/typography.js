c08452a5c78b6fc0203116e455544187ccd6baae
Erik Aybar <erikthedeveloper@gmail.com>
2019-05-01 06:43:59 -0600

    typography.js Reduce scale. Fix mobile font-size.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,17 @@ import Typography from 'typography'
 import Theme from 'typography-theme-moraga'
 import '../styles/code-syntax-night-owl.css'
 
+Theme.baseFontSize = '22px'
+Theme.scaleRatio = 2
+Theme.overrideThemeStyles = () => ({
+  '@media only screen and (max-width: 768px)': {
+    html: {
+      fontSize: '125%',
+      lineHeight: '26px',
+    },
+  },
+})
+
 const typography = new Typography(Theme)
 
 // Hot reload typography in development.
